68bf1f63e7e9b1d09daa5e2afbff5a3c3971666b
Takashi Abe <tabe@salesforce.com>
2016-09-29 17:08:26 +0900

    Using sinatra and web UI

--- json2table.rb
+++ json2table.rb
@@ -1,9 +1,11 @@
 #!/usr/bin/env ruby
 # coding: utf-8
 
-require 'json'
+require "sinatra"
+require "json"
 
 # JSON テストデータ
+=begin
 test_json = '{"widget": {
 "debug": "on",
 "window": {
@@ -30,37 +32,34 @@ test_json = '{"widget": {
   "onMouseUp": "sun1.opacity = (sun1.opacity / 100) * 90;"
 }
 }}'
+=end
 
-test_hash = JSON.parse(test_json)
+get '/' do
+  erb :index
+end
 
-def print_keys(hash, nest = 0)
+post '/' do
+  @hash = JSON.parse(params['json'])
+  erb :json2table
+end
+
+# test_hash = JSON.parse(test_json)
+
+def print_keys(hash, res = "")
   hash.each do |key, val|
     if val.is_a?(Hash) then
       # puts "#{'　' * (nest * 3)} #{key}</br>"
       # puts "<tr><td>#{key}</td></tr>\n<tr><td>\n<table>"
-      puts "<tr><th>#{key}</th><th><table class=\"table is-bordered\">"
-      print_keys(val, nest + 1)
-      puts "</th></table>"
+      res += "<tr><th>#{key}</th><th><table class=\"table is-bordered\">"
+      res = print_keys(val, res)
+      res += "</th></table>"
       # puts "</td></tr>\n</table>"
       # puts '</div>'
     else
-      puts "<tr><td>#{key}</td><td>#{val}</td></tr>"
+      res += "<tr><td>#{key}</td><td>#{val}</td></tr>"
       # puts "#{'　' * (nest * 3)} #{key}: #{val}</br>"
       # puts "<div class=\"columns\">\n<div class=\"column\">#{key}</div>\n<div class=\"column\">#{val}</div>\n</div>"
     end
   end
+  return res
 end
-
-puts '<!DOCTYPE HTML>
-<html>
-<head>
-<meta charset="utf-8" />
-<title>KOMATSU POC</title>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.1.2/css/bulma.min.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
-</head>
-<body><div class="container"><table class="table">'
-
-print_keys(test_hash)
-
-puts '</table></div></body></html>'
